41637d606e6cb62bd60bc3fa1f45bcc8a4771c59
shubham-shootorder <shubham@shootorder.in>
2025-06-04 19:18:01 +0530

    change the order of about us section

--- src/components/Home/About/AboutUs.jsx
+++ src/components/Home/About/AboutUs.jsx
@@ -29,34 +29,43 @@ export default function AboutUs() {
                 About Us
               </h4>
               <h2 className="text-3xl md:text-4xl font-bold text-gray-900">
-               	ShootOrder - Digital Marketing Agency
+                ShootOrder - Digital Marketing Agency
               </h2>
             </div>
 
             <div className="space-y-4 text-gray-600">
               <p className="leading-relaxed">
-                ShootOrder is a leading full-service digital marketing agency helping businesses across the United States grow smarter and faster.
+                ShootOrder is a leading full-service digital marketing agency
+                helping businesses across the United States grow smarter and
+                faster.
               </p>
               <p className="leading-relaxed">
-               Trusted by over 300 businesses worldwide, we&apos;ve built a strong reputation for delivering real results through proven strategies in Content Marketing, SEO, PPC Advertising, and Social Media Marketing.
+                Trusted by over 300 businesses worldwide, we&apos;ve built a
+                strong reputation for delivering real results through proven
+                strategies in Content Marketing, SEO, PPC Advertising, and
+                Social Media Marketing.
               </p>
               <p className="leading-relaxed">
-                Our team has been instrumental in scaling numerous internet startups, helping them transform into industry leaders. With our growing presence, we’re continuing to expand our impact across the U.S., bringing global expertise with a local edge.
+                Our team has been instrumental in scaling numerous internet
+                startups, helping them transform into industry leaders. With our
+                growing presence, we’re continuing to expand our impact across
+                the U.S., bringing global expertise with a local edge.
               </p>
             </div>
 
             <div className="grid grid-cols-3 gap-8 pt-6">
               <div className="space-y-2 border-1 border-gray-300 p-3 rounded-xl text-center">
-                <h3 className="text-3xl font-bold text-[#9a0c28]">300+</h3>
-                <p className="text-gray-800 font-medium">Global Clients</p>
+                <h3 className="text-3xl font-bold text-[#9a0c28]">12 +</h3>
+                <p className="text-gray-800 font-medium">Years Of Experience</p>
               </div>
+
               <div className="space-y-2 border-1 border-gray-300 p-3 rounded-xl text-center">
                 <h3 className="text-3xl font-bold text-[#9a0c28]">2</h3>
                 <p className="text-gray-800 font-medium">Major Locations</p>
               </div>
               <div className="space-y-2 border-1 border-gray-300 p-3 rounded-xl text-center">
-                <h3 className="text-3xl font-bold text-[#9a0c28]">12 +</h3>
-                <p className="text-gray-800 font-medium">Years Of Experience</p>
+                <h3 className="text-3xl font-bold text-[#9a0c28]">300+</h3>
+                <p className="text-gray-800 font-medium">Global Clients</p>
               </div>
             </div>
           </div>
